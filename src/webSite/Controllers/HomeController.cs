e02310c228c099673a3e0361bcd5c4a72690d09b
victor <supertigerzou@gmail.com>
2016-02-12 00:27:07 +0800

    the first version of website

--- src/webSite/Controllers/HomeController.cs
+++ src/webSite/Controllers/HomeController.cs
@@ -13,17 +13,15 @@ namespace webSite.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult BookStore()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "听故事、读好书，老虎工作室依托于电台几十万粉丝，和各大出版社合作推出了适合于各个年龄阶段的优质童书以及适合于父母阅读的各类儿童教育书籍，以最合理的价格，带给您和您家宝贝不一样的精彩。进入书店分两种模式，逛街模式适用于按书籍类别随意浏览，搜索模式适用于按特定需求精准定位。";
 
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult RadioStation()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
