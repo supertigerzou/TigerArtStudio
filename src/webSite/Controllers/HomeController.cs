ca3570d8c4de8f2e40517d14857740b41fd66ed8
victor <supertigerzou@gmail.com>
2016-02-13 19:52:10 +0800

    refine things

--- src/webSite/Controllers/HomeController.cs
+++ src/webSite/Controllers/HomeController.cs
@@ -25,6 +25,13 @@ namespace webSite.Controllers
             return View();
         }
 
+        public IActionResult Community()
+        {
+            ViewData["Message"] = "老虎工作室社区依托于微信平台，旨在为爱学习爱分享的家长建立一个互相分享互相学习的平台。目前已经了汇集了全国各地数万名爱听故事、爱阅读的孩子的家长，每天在群内就孩子的成长各抒己见，话题涉及学习娱乐生活的方方面面。";
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View();
